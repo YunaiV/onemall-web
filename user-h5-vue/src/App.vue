d0a326a8ab24ecd0df7416d9a5a14112ed77c195
YunaiV <>
2020-07-11 21:15:01 +0800

    onemall 前端项目提交

---
+++ user-h5-vue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import './assets/style/common.css';
+export default {
+  name: 'app'
+}
+</script>
+
+
---
+++ user-h5-vue/src/page/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <page/>
+        <navigate />
+    </div>
+</template>
+
+<script>
+import navigate from '../components/footer/navigate.vue'
+import page from './page/index.vue'
+
+export default {
+    components:{
+        page,
+        navigate
+    }
+}
+</script>
+<style>
+
+</style>
