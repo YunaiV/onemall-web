521f22898d8a60475729c970c18bc2cbe3e4cd37
YunaiV <>
2020-11-26 18:55:59 +0800

    尝试 staging 环境，使用七牛 CDN 加速

--- admin-dashboard-vue/vue.config.js
+++ admin-dashboard-vue/vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : '/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
